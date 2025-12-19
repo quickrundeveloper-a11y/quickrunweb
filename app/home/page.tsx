aecaddf8fa7d7ed56825db4efbd06a0e6f8c9299
Sachin <sanfet7@gamil.com>
2025-12-19 10:45:26 +0530

    aall doone

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -6,11 +6,14 @@ export const metadata = {
     "Quickrun Delivers Fresh Groceries And Daily Essentials To Your Doorstep Within Minutes. Enjoy Fast, Reliable, And Convenient Online Shopping Anytime You Need It.",
   keywords: [
     "Online Grocery Delivery",
-     "Fresh Fruits and Vegetables",
-      "Daily Essentials Online",
-       "Quick Grocery Delivery",
-        "Buy Groceries Near Me",
+    "Fresh Fruits and Vegetables",
+    "Daily Essentials Online",
+    "Quick Grocery Delivery",
+    "Buy Groceries Near Me",
   ],
+  alternates: {
+    canonical: "https://www.quickrunfast.com/home",
+  },
 };
 
 export default function Page() {
--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+
+      {/* ✅ Google Tag Manager */}
+<Script
+  id="gtm"
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MPMJ33XW');`,
+  }}
+/>
+
+
         {/* ⭐ GMB / LocalBusiness Schema */}
         <script
           type="application/ld+json"
@@ -107,25 +122,19 @@ export default function RootLayout({
 `,
           }}
         />
-
-        
-  {/* ⭐ Google Analytics */}
-  <Script
-    src="https://www.googletagmanager.com/gtag/js?id=G-NHVZ4WLK8S"
-    strategy="afterInteractive"
-  />
-
-  <Script id="google-analytics" strategy="afterInteractive">
-    {`
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-NHVZ4WLK8S');
-    `}
-  </Script>
       </head>
 
       <body className={lexend.className}>
+        
+          {/* ✅ Google Tag Manager (noscript) */}
+  <noscript>
+    <iframe
+      src="https://www.googletagmanager.com/ns.html?id=GTM-MPMJ33XW"
+      height="0"
+      width="0"
+      style={{ display: "none", visibility: "hidden" }}
+    />
+  </noscript>
         <LocationProvider>
   <ClientLayout>{children}</ClientLayout>
 </LocationProvider>
